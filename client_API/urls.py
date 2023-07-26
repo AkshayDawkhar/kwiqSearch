be5bd5756e10a0c724b18ca9360edddcf1e95bd9
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-07-26 22:36:50 +0530

    change urls of clients -> client

--- client_API/urls.py
+++ client_API/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ClientAPIView, SearchFilterAPIView, FollowUpAPIView, FeedbackAPIView, FollowUpDate,ClientDetailsAPIView
 
 urlpatterns = [
-    path('clients/', ClientAPIView.as_view(), name='client-list'),
+    path('client/', ClientAPIView.as_view(), name='client-list'),
     path('searchfilters/', SearchFilterAPIView.as_view(), name='searchfilter-list'),
     path('followups/', FollowUpAPIView.as_view(), name='followup-list'),
     path('feedbacks/', FeedbackAPIView.as_view(), name='feedback-list'),
